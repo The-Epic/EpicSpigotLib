61883f42c8d8f79c97d0b7ececaf92322137e309
Epic <87966707+The-Epic@users.noreply.github.com>
2022-12-06 00:43:22 +0000

    Update .gitignore, add UUIDSerialization.java

---
+++ src/main/java/me/epic/spigotlib/storage/UUIDSerialization.java
@@ -0,0 +1,19 @@
+package me.epic.spigotlib.storage;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UUIDSerialization {
+    public static byte[] getBytesFromUUID(UUID uuid) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+
+        return bb.array();
+    }
+
+    public static UUID getUUIDFromBytes(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+}
