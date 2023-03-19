79137aa76908ade15e2fb15ddfeb39d855640b1c
The-Epic <EpicEbic@outlook.com>
2023-03-19 15:41:05 +0000

    other 1.18 module

---
+++ spigot-1_18_R2/src/main/java/me/epic/spigotlib/nms/NMSAdapter.java
@@ -0,0 +1,36 @@
+package me.epic.spigotlib.nms;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_18_R2.CraftServer;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
+
+public class NMSAdapter implements INMSAdapter {
+
+    private static final String TEXTURE_URL = "http://textures.minecraft.net/texture/";
+
+
+    @Override
+    public void setTexture(SkullMeta meta, String texture) {
+        PlayerProfile profile = Bukkit.createPlayerProfile(UUID.nameUUIDFromBytes(texture.getBytes()));
+        PlayerTextures textures = profile.getTextures();
+        try {
+            textures.setSkin(new URL(TEXTURE_URL + texture));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        profile.setTextures(textures);
+        meta.setOwnerProfile(profile);
+    }
+
+    @Override
+    public String getDefaultWorldName() {
+        return ((CraftServer) Bukkit.getServer()).getServer().getProperties().levelName;
+    }
+}
