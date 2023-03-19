8237f3260526d9a5c3fea25d7ce6c6ba0e5f337c
The-Epic <EpicEbic@outlook.com>
2023-03-19 18:49:25 +0000

    fix error

--- epicspigotlib-core/src/main/java/me/epic/spigotlib/commands/SimpleCommandHandler.java
+++ epicspigotlib-core/src/main/java/me/epic/spigotlib/commands/SimpleCommandHandler.java
@@ -17,21 +17,24 @@ public abstract class SimpleCommandHandler implements CommandExecutor, TabComple
 	}
 
 	/**
-	 *
-	 * @param sender Source of the command.  For players tab-completing a
-	 *     command inside a command block, this will be the player, not
-	 *     the command block.
+	 * @param sender  Source of the command.  For players tab-completing a
+	 *                command inside a command block, this will be the player, not
+	 *                the command block.
 	 * @param command Command which was executed
-	 * @param alias Alias of the command which was used
-	 * @param args The arguments passed to the command, including final
-	 *     partial argument to be completed
+	 * @param alias   Alias of the command which was used
+	 * @param args    The arguments passed to the command, including final
+	 *                partial argument to be completed
 	 * @return StringList
 	 */
 	@Override
-	public abstract List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args);
+	public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+		return null;
+	}
 
 	@Override
-	public abstract boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args);
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+		return false;
+	}
 
 	/**
 	 * Gets the permission for the command
